ca735570ef9238775ca16f5da2ba6fee315875bc
Ankit Desai <desaiankitb@gmail.com>
2023-07-27 12:26:58 +0530

    adding docstrings for all the functions and classes

--- src/configutility.py
+++ src/configutility.py
@@ -1,15 +1,45 @@
 import configparser
 
 class ConfigUtility:
+    """
+    Utility class to manage configurations from a given configuration file.
+    
+    Attributes:
+        config (ConfigParser): An instance of ConfigParser to read the configuration file.
+    """
+    
     def __init__(self, config_file='src/config.ini'):
+        """
+        Initializes the ConfigUtility with a given configuration file.
+        
+        Args:
+            config_file (str): Path to the configuration file. Defaults to 'src/config.ini'.
+        """
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
 
     def get_mongo_config(self):
+        """
+        Fetches the MongoDB configuration from the configuration file.
+        
+        Returns:
+            dict: A dictionary containing MongoDB configurations such as HOST, PORT, USERNAME, PASSWORD, and DB_NAME.
+        """
         return {
             'HOST': self.config.get('MONGODB', 'HOST', fallback='localhost'),
             'PORT': self.config.getint('MONGODB', 'PORT', fallback=27017),
             'USERNAME': self.config.get('MONGODB', 'USERNAME'),
             'PASSWORD': self.config.get('MONGODB', 'PASSWORD'),
             'DB_NAME': self.config.get('MONGODB', 'DB_NAME', fallback='ds_trends_db')
+        }
+    
+    def get_slack_config(self):
+        """
+        Fetches the Slack configuration from the configuration file.
+        
+        Returns:
+            dict: A dictionary containing the Slack webhook URL.
+        """
+        return {
+            'SLACK_WEBHOOK_URL': self.config.get('SLACK', 'SLACK_WEBHOOK_URL')
         }
