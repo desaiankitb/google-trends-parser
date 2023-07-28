a8a111054aa806b22ca8d3600ba2785505ff091c
Ankit Desai <desaiankitb@gmail.com>
2023-07-28 10:35:55 +0530

    adding logging in SlackNotification

--- src/SlackNotifier.py
+++ src/SlackNotifier.py
@@ -2,6 +2,7 @@ import requests
 from src.google_trends_parser import parse_google_trends_rss
 from src.google_trends_to_mongodb import store_in_mongodb
 from src.configutility import ConfigUtility
+from src.logging_config import LoggerConfig
 
 class SlackNotifier:
     """
@@ -19,6 +20,8 @@ class SlackNotifier:
             webhook_url (str): The Slack webhook URL.
         """
         self.webhook_url = webhook_url
+        self.logger = LoggerConfig().get_logger()
+        self.logger.info("SlackNotifier initialized with webhook URL.")
 
     def send_message(self, message):
         """
@@ -32,13 +35,21 @@ class SlackNotifier:
         """
         payload = {"text": message}
         response = requests.post(self.webhook_url, json=payload)
-        return response.status_code == 200
+        if response.status_code == 200:
+            self.logger.info(f"Message sent to Slack: {message}")
+            return True
+        else:
+            self.logger.error(f"Failed to send message to Slack. Status code: {response.status_code}")
+            return False
 
 if __name__ == "__main__":
     """
     Main execution point. Parses the Google Trends RSS feed, extracts the trending data,
     stores it into a MongoDB collection, and sends a notification to Slack about the operation's status.
     """
+    
+    logger = LoggerConfig().get_logger()
+    logger.info("Starting the main execution.")
     
     # Fetch Slack webhook URL using ConfigUtility
     config_util = ConfigUtility()
@@ -51,5 +62,7 @@ if __name__ == "__main__":
         trends = parse_google_trends_rss(url)
         store_in_mongodb(trends)
         notifier.send_message("Trends stored in MongoDB successfully!")
+        logger.info("Trends stored in MongoDB and Slack notification sent.")
     except Exception as e:
         notifier.send_message(f"Error occurred: {str(e)}")
+        logger.error(f"Error occurred during main execution: {str(e)}")
