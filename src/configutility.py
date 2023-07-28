fbfacd58ea20cb25fe51eaedf6b49cc54dbd3ea8
Ankit Desai <desaiankitb@gmail.com>
2023-07-28 10:36:20 +0530

    adding logging in config utils

--- src/configutility.py
+++ src/configutility.py
@@ -1,4 +1,5 @@
 import configparser
+from src.logging_config import LoggerConfig
 
 class ConfigUtility:
     """
@@ -6,6 +7,7 @@ class ConfigUtility:
     
     Attributes:
         config (ConfigParser): An instance of ConfigParser to read the configuration file.
+        logger (Logger): Logger instance for logging.
     """
     
     def __init__(self, config_file='src/config.ini'):
@@ -17,6 +19,10 @@ class ConfigUtility:
         """
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
+        
+        # Initialize logger
+        self.logger = LoggerConfig().get_logger()
+        self.logger.info(f"ConfigUtility initialized with config file: {config_file}")
 
     def get_mongo_config(self):
         """
@@ -25,6 +31,7 @@ class ConfigUtility:
         Returns:
             dict: A dictionary containing MongoDB configurations such as HOST, PORT, USERNAME, PASSWORD, and DB_NAME.
         """
+        self.logger.info("Fetching MongoDB configuration from config file.")
         return {
             'HOST': self.config.get('MONGODB', 'HOST', fallback='localhost'),
             'PORT': self.config.getint('MONGODB', 'PORT', fallback=27017),
@@ -40,6 +47,7 @@ class ConfigUtility:
         Returns:
             dict: A dictionary containing the Slack webhook URL.
         """
+        self.logger.info("Fetching Slack configuration from config file.")
         return {
             'SLACK_WEBHOOK_URL': self.config.get('SLACK', 'SLACK_WEBHOOK_URL')
         }
